68731104607f63256579a9ad4a58f8433311ef1d
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2023-02-15 10:07:59 -0500

    ZW's initial script

---
+++ Graphing_Orbit_Plots.m
@@ -0,0 +1,40 @@
+r = (-3:0.0002:3)';
+
+%define the iteration value
+
+itr = 10^3;
+
+x = zeros(length(r), itr);
+
+%add the initial value of the function
+
+x0 = 5;
+
+%define the function
+
+x(:,1) = r.*cos(x0);
+
+for i = 150:itr-1
+
+%function id defined over here
+
+x(:,i+1) = r.*cos(x(:,i));
+
+end
+
+%Plot the iteration value
+
+plot(r, x, '.', 'MarkerSize', 1);
+
+% To add title of the plot
+
+title('x verse r')
+
+%to label the axis
+
+xlabel('r');ylabel('x');
+
+hold on
+
+grid on;
+hold off
